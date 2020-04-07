4f6a3dd8558d8f967bec10ea46b1169653413c3e
pressstartplayz <56609591+pressstartgaming@users.noreply.github.com>
2020-04-07 09:13:12 -0500

    Update SettingsOverlay.vue

--- src/components/overlays/SettingsOverlay.vue
+++ src/components/overlays/SettingsOverlay.vue
@@ -2,7 +2,7 @@
   <div>
     <h3>Settings</h3>
     <div><label>Key Offset <input type="number" v-model.number="options.keyOffset"></label></div>
-    <button @click="hide">Close</button>
+    <button @click="hide"> Save settings and Close</button>
   </div>
 </template>
 
