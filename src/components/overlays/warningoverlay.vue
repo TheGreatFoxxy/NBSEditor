f660ba185b24870e9f60678f3eac8958ac7fc113
pressstartplayz <56609591+pressstartgaming@users.noreply.github.com>
2020-08-03 09:35:56 -0500

    Create Warning Overlay

---
+++ src/components/overlays/warningoverlay.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3>!!WARNING!!</h3>
+    <p>Now we may have new instruments, and its cool that we do, but using them will render your song uncompatible with minecraft!</p>
+    <p>USE AT YOUR WORN RISK!</p>
+    <button @click="hide">Acknowledge and close</button>
+  </div>
+</template>
+
+<script>
+export default {
+  inject: ["hide"],
+  props: {
+    options: Object,
+  },
+}
+</script>
+
+<style>
+h3 {
+color: white;
+background-color: red;
+font-family: sans-serif;
+}
+</style>
