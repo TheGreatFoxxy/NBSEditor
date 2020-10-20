ca81e56e7e2bfc0b94263e84ce8ffa4fbc00b6a2
TheGreatFoxxy <56609591+TheGreatFoxxy@users.noreply.github.com>
2020-10-20 16:28:41 -0500

    Create File for Auxillary functions

---
+++ js/auxillary.js
@@ -0,0 +1,2 @@
+// Function for reloading/updating the page
+function reloadPageFunction() { if(confirm("You are about to update/refresh the page. Be sure to save, as you will lose any progress you have made!")){ location.reload(); }},
