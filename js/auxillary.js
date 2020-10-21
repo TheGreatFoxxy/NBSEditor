d680b6a0086a81db3fcdd1ad76a91c7a92b7b5c0
TheGreatFoxxy <56609591+TheGreatFoxxy@users.noreply.github.com>
2020-10-21 16:23:31 -0500

    Removed the need for auxillary.js

--- js/auxillary.js
+++
@@ -1,2 +0,0 @@
-// Function for reloading/updating the page
-function reloadPageFunction() { if(confirm("You are about to update/refresh the page. Be sure to save, as you will lose any progress you have made!")){ location.reload(); }},
